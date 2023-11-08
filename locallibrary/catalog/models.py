477fc0c0cbedfe373fee49a8e1ed473b1ee55368
opezdola <nekras.2001@mail.ru>
2023-11-08 22:11:56 +0300

    vse

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,38 +1,35 @@
 from django.db import models
 from django.urls import reverse
 import uuid
+from django.contrib.auth.models import User
+from datetime import date
 
 class Genre(models.Model):
     """Model representing a book genre."""
     name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
 
-    def str(self):
+    def __str__(self):
         """String for representing the Model object."""
         return self.name
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
-
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author is a string rather than an object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, unique=True,
                              help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
-    def str(self):
+    def __str__(self):
         """String for representing the Model object."""
         return self.title
 
     def get_absolute_url(self):
-        """Returns the URL to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
+            """Returns the url to access a particular book instance."""
+            return reverse('catalog:book-detail', args=[str(self.id)])
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -40,6 +37,12 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -59,7 +62,7 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ['due_back']
 
-    def str(self):
+    def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
 
@@ -73,10 +76,10 @@ class Author(models.Model):
     class Meta:
         ordering = ['last_name', 'first_name']
 
-    def get_absolute_url(self):
-        """Returns the URL to access a particular author instance."""
-        return reverse('author-detail', args=[str(self.id)])
+    # def get_absolute_url(self):
+    #     """Returns the URL to access a particular author instance."""
+    #     return reverse('catalog:author-detail', args=[str(self.id)])
 
-    def str(self):
+    def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
